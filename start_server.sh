feb5daa00d9fb32311f6fb98b07c783f4b9a1a8b
vaish-0 <98145252+vaish-0@users.noreply.github.com>
2022-05-12 15:50:25 +0530

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
-
-# navigate to app folder
-cd app
-
-# initial startup by running react-script "start", name process "marketing"
-# --watch watches and restarts if files change
-pm2 start ./node_modules/react-scripts/scripts/start.js --name "marketing" --watch
-
-# auto restart server if shut down
+cd /home/ec2-user/server/src
+npm start
+pm2 start npm --name "covidapp" -- start
 pm2 startup
-
-# freeze process list for automatic respawn
 pm2 save
-
-# restart all processes - necessary to do this again?
 pm2 restart all
